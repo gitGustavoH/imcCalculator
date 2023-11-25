e591ec7e8b8c0690262bdc559bd2b01f0ccf2ea0
Gustavo Henrique <gustavohenriquecns.wk@gmail.com>
2023-11-25 12:25:41 -0800

    The main activity was debug

--- app/src/main/java/com/gustavohenrique/projetocalculoimc/MainActivity.kt
+++ app/src/main/java/com/gustavohenrique/projetocalculoimc/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.gustavohenrique.projetocalculoimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btnCalcular)
+
+        btnCalcular.setOnClickListener {
+            val i = Intent(this, ResultadoActivity::class.java)
+
+            startActivity(i)
+        }
     }
 }
